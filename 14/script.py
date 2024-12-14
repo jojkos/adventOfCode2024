6e56abd71cc1ec0d89853402f2215da3a03813a0
Jonas Holcner <jojkos@gmail.com>
2024-12-14 12:51:59 +0100

    fix part 1

--- 14/script.py
+++ 14/script.py
@@ -1,4 +1,3 @@
-
 class Map:
     movement = {
         "left": (0, -1),
@@ -13,23 +12,22 @@ class Map:
         self.width = width
         self.height = height
         self.map = [[self.Empty for y in range(width)] for x in range(height)]
-        
+
         for robot in robots:
             posX, posY = robot["pos"]
             velX, velY = robot["vel"]
-            valAtPos = self.map[posY][posX]
+            valAtPos = self.map[posX][posY]
 
             if valAtPos == self.Empty:
-                self.map[posY][posX] = []
-            
-            self.map[posY][posX].append((velX, velY))
+                self.map[posX][posY] = []
 
-        
-    def print(self, ignoreMiddle = False):
+            self.map[posX][posY].append((velX, velY))
+
+    def print(self, ignoreMiddle=False):
         map = self.map
 
-        xMid = round(self.height / 2) - 1
-        yMid = round(self.width / 2) - 1
+        xMid = self.height // 2
+        yMid = self.width // 2
 
         for x in range(len(map)):
             for y in range(len(map[0])):
@@ -47,50 +45,36 @@ class Map:
     def isInsideMapBounds(self, pos, map):
         (x, y) = pos
         return x >= 0 and x < len(map) and y >= 0 and y < len(map[0])
-    
-    def move(self):
+
+    def move(self, robots, count):
         map = self.map
         newMap = [[self.Empty for y in range(len(map[0]))] for x in range(len(map))]
 
-        for x in range(len(map)):
-            for y in range(len(map[0])):
-                if map[x][y] != self.Empty:
-                    for vel in map[x][y]:
-                        (velX, velY) = vel
-                        newPosX, newPosY = (x + velY, y + velX)
+        for robot in robots:
+            posX, posY = robot["pos"]
+            velX, velY = robot["vel"]
+            newPosX, newPosY = (posX + (velX * count), posY + (velY * count))
 
-                        newPosX = newPosX % len(map)
-                        newPosY = newPosY % len(map[0])
+            newPosX = newPosX % len(map)
+            newPosY = newPosY % len(map[0])
 
-                        if newMap[newPosX][newPosY] == self.Empty:
-                            newMap[newPosX][newPosY] = []
+            if newMap[newPosX][newPosY] == self.Empty:
+                newMap[newPosX][newPosY] = []
 
-                        newMap[newPosX][newPosY].append(vel)
+            newMap[newPosX][newPosY].append(vel)
 
         self.map = newMap
 
     def countRobotsInQuadrants(self):
-        xMid = round(self.height / 2) - 1
-        yMid = round(self.width / 2) - 1
-        # xMidMin = round(self.height / 2) - 1
-        # xMidMax = xMidMin
-        # yMidMin = round(self.width / 2) - 1
-        # yMidMax = yMidMin
-
-        # if self.width % 2 == 0:
-        #     yMidMax += 1
-        # if self.height % 2 == 0:
-        #     xMidMax += 1
-
-        quadrants = [0,0,0,0]
+        xMid = self.height // 2
+        yMid = self.width // 2
+        quadrants = [0, 0, 0, 0]
 
         for x in range(len(self.map)):
             for y in range(len(self.map[0])):
                 if x == xMid or y == yMid:
                     continue
-                # if x >= xMidMin and x <= xMidMax or y >= yMidMin and y <= yMidMax: 
-                #     continue
-                
+
                 if x < xMid and y < yMid:
                     quadrantIndex = 0
                 elif x < xMid and y > yMid:
@@ -102,7 +86,7 @@ class Map:
 
                 if self.map[x][y] != self.Empty:
                     quadrants[quadrantIndex] += len(self.map[x][y])
-                
+
         res = quadrants[0] * quadrants[1] * quadrants[2] * quadrants[3]
 
         print(quadrants)
@@ -110,35 +94,29 @@ class Map:
 
 
 with open("input.txt", "r") as file:
-        # read all lines, remove new line character
+    # read all lines, remove new line character
     input = [line.strip() for line in file.readlines()]
 
-width = 11
-height = 7
-# width = 101
-# height = 103
+# width = 11
+# height = 7
+width = 101
+height = 103
 robots = []
 
 for line in input:
-    pos,vel = line.split(" ")
+    pos, vel = line.split(" ")
     pos = pos.split("=")[1]
-    posX, posY = [int(i) for i in pos.split(",")]
+    posY, posX = [int(i) for i in pos.split(",")]
     vel = vel.split("=")[1]
-    velX,velY = [int(i) for i in vel.split(",")]
+    velY, velX = [int(i) for i in vel.split(",")]
 
-    robots.append({
-        "pos": (posX, posY),
-        "vel": (velX, velY)
-    })
+    robots.append({"pos": (posX, posY), "vel": (velX, velY)})
 
 map = Map(width, height, robots)
 
 t = 100
 
-# map.print()
-for i in range(t):
-    map.move()
-    # map.print()
-
-map.print(True)
+# map.print(True)
+map.move(robots, t)
+# map.print(True)
 map.countRobotsInQuadrants()
