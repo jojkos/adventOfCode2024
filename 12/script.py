6ffa3386e027a60d5f260602cbbab4cac65f6b91
Jonáš Holcner <jonas.holcner@solitea.cz>
2024-12-15 15:59:49 +0100

    finish day 12 part 2

--- 12/script.py
+++ 12/script.py
@@ -6,6 +6,32 @@ class Map:
         "down": (1, 0),
     }
 
+    opposites = {
+        "left": "right",
+        "right": "left",
+        "up": "down",
+        "down": "up",
+    }
+
+    # neighbors = {
+    #     "left": ["up", "down"],
+    #     "right": ["up", "down"],
+    #     "up": ["left", "right"],
+    #     "down": ["left", "right"],
+    # }
+
+    # vertexCombination = [
+    #     ["up", "left"], ["up", "right"], ["down", "left"], ["down", "right"],
+    #     ["up", "left", "down" ], ["up", "right", "down" ], ["up", "left", "right" ], ["left", "down", "right" ]
+    # ]
+
+    diagonals = {
+        "up-left": (-1, -1),
+        "up-right": (-1, 1),
+        "down-left": (1, -1),
+        "down-right": (1, 1),
+    }
+
     def __init__(
         self,
     ):
@@ -29,6 +55,8 @@ class Map:
     def getPerimeter(self, pos):
         x, y = pos
         perimeter = 0
+        oppositeOnly = False
+        firstFound = False
 
         for dir in self.movement:
             dx, dy = self.movement[dir]
@@ -40,10 +68,32 @@ class Map:
             ):
                 perimeter += 1
 
-        return perimeter
+                if not firstFound:
+                    firstFound = dir
+                else:
+                    opposite = self.opposites[firstFound]
+
+                    if dir == opposite:
+                        oppositeOnly = True
+
+        return {
+            "perimeter": perimeter,
+            "opposites": oppositeOnly,
+        }
+    
+    def posHasNeighbor(self, pos,dt):
+        x, y = pos
+        val = self.map[x][y]
+        dx, dy = dt
+        newX, newY = x + dx, y + dy
+
+        if not self.isInsideMapBounds((newX, newY)):
+            return False
+
+        return self.map[newX][newY] == val
 
     def findRegions(self):
-        visited = []
+        visited = {}
         regions = []
 
         for x in range(len(self.map)):
@@ -56,16 +106,46 @@ class Map:
                 stack = [(x, y)]
                 area = 0
                 perimeter = 0
+                vertices = 0
 
-                visited.append(key)
+                visited[key] = True
 
                 while len(stack) > 0:
                     currentX, currentY = stack.pop()
-                    currentKey = f"{currentX},{currentY}"
                     currentVal = self.map[currentX][currentY]
 
                     area += 1
-                    perimeter += self.getPerimeter((currentX, currentY))
+                    currentPerimeter = self.getPerimeter((currentX, currentY))
+                    perimeter += currentPerimeter["perimeter"]
+                    currentVertices = 0
+
+                    if currentPerimeter["perimeter"] == 4:
+                        currentVertices = 4
+                    elif currentPerimeter["perimeter"] == 2:
+                        if not currentPerimeter["opposites"]:
+                            currentVertices = 1
+                    elif currentPerimeter["perimeter"] > 1:
+                        newVertices = currentPerimeter["perimeter"] - 1
+                        currentVertices = newVertices
+
+                    found = 0
+
+                    if self.posHasNeighbor((currentX, currentY), self.movement["up"]) and self.posHasNeighbor((currentX, currentY), self.movement["left"]):
+                        if not self.posHasNeighbor((currentX, currentY), self.diagonals["up-left"]):
+                            found += 1
+                    if self.posHasNeighbor((currentX, currentY), self.movement["up"]) and self.posHasNeighbor((currentX, currentY), self.movement["right"]):
+                        if not self.posHasNeighbor((currentX, currentY), self.diagonals["up-right"]):
+                            found += 1
+                    if self.posHasNeighbor((currentX, currentY), self.movement["down"]) and self.posHasNeighbor((currentX, currentY), self.movement["left"]):
+                        if not self.posHasNeighbor((currentX, currentY), self.diagonals["down-left"]):
+                            found += 1
+                    if self.posHasNeighbor((currentX, currentY), self.movement["down"]) and self.posHasNeighbor((currentX, currentY), self.movement["right"]):
+                        if not self.posHasNeighbor((currentX, currentY), self.diagonals["down-right"]):
+                            found += 1
+
+                    currentVertices += found
+
+                    vertices += currentVertices
 
                     for dir in self.movement:
                         dx, dy = self.movement[dir]
@@ -84,13 +164,18 @@ class Map:
                             continue
 
                         stack.append((newX, newY))
-                        visited.append(newKey)
+                        visited[newKey] = True
 
                 regions.append(
-                    {"area": area, "perimeter": perimeter, "val": currentVal}
+                    {
+                        "area": area,
+                        "perimeter": perimeter,
+                        "vertices": vertices,
+                        "val": currentVal,
+                    }
                 )
 
-        print(regions)
+        # print(regions)
 
         return regions
 
@@ -100,8 +185,11 @@ field = Map()
 regions = field.findRegions()
 
 price = 0
+price2 = 0
 
 for region in regions:
     price += region["area"] * region["perimeter"]
+    price2 += region["area"] * region["vertices"]
 
 print(price)
+print(price2)
