14d95bb787a89a10520be9b3638f9a88ed7b15f2
Jonas Holcner <jojkos@gmail.com>
2024-12-24 11:24:00 +0100

    improve

--- 23/script.py
+++ 23/script.py
@@ -1,7 +1,9 @@
 from itertools import combinations, permutations
 
+
 def getAllCombinations(lst, N):
     return list(combinations(lst, N))
+
 
 with open("input.txt", "r") as file:
     input = file.readlines()
@@ -17,17 +19,20 @@ for line in input:
         nodes[n1] = []
     if n2 not in nodes:
         nodes[n2] = []
-    
+
     nodes[n1].append(n2)
     nodes[n2].append(n1)
+
 
 def getGroups(groupSize):
     groups = []
     c = 0
+    nodesSize = len(nodes)
 
     for node in nodes:
-        possibleGroups = getAllCombinations([node] + nodes[node], groupSize)        
-        print(c)
+        possibleGroups = getAllCombinations([node] + nodes[node], groupSize)
+        print(f"{c}/{nodesSize}")
+
         for possibleGroup in possibleGroups:
             isGroup = True
             sortedPossibleGroup = sorted(possibleGroup)
@@ -36,9 +41,6 @@ def getGroups(groupSize):
             if key in groups:
                 continue
 
-            if (sortedPossibleGroup[0] == "aq") and sortedPossibleGroup[1]  == "cg" and sortedPossibleGroup[2] == "yn":
-                print(sortedPossibleGroup)
-            
             for i in range(len(sortedPossibleGroup)):
                 for j in range(1, len((sortedPossibleGroup))):
                     n1 = sortedPossibleGroup[i]
@@ -56,33 +58,36 @@ def getGroups(groupSize):
 
     return groups
 
+
 def findBiggestGroup():
     maxNodes = 0
 
     for node in nodes:
         childrenLen = len(nodes[node])
-        
+
         if childrenLen > maxNodes:
             maxNodes = childrenLen
-            
-    groups = getGroups(maxNodes)
-    print("res2", groups)
 
-# groups = getGroups(3)
-# # print(len(groups))
-# # print(groups)
-# sum = 0
+    biggestGroup = getGroups(maxNodes)
 
-# for group in groups:
-#     nodes = group.split(",")
-#     tFound = False
+    return biggestGroup
 
-#     for node in nodes:
-#         if node.startswith("t"):
-#             tFound = True
-#             break
-#     if tFound:
-#         sum += 1
-# print("res1", sum)
 
-findBiggestGroup()
+groups = getGroups(3)
+sum = 0
+
+for group in groups:
+    tmpNodes = group.split(",")
+    tFound = False
+
+    for node in tmpNodes:
+        if node.startswith("t"):
+            tFound = True
+            break
+    if tFound:
+        sum += 1
+
+biggestGroup = findBiggestGroup()
+
+print("res1", sum)
+print("res2", biggestGroup[0])
